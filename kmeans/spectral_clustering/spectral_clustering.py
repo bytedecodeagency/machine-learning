926cd83e4a2e75ffe2dc97a442dac4f3706549c9
JoHN <joandesonandradesilva@gmail.com>
2021-11-03 03:46:05 -0300

    Spectral Clustering

---
+++ kmeans/spectral_clustering/spectral_clustering.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+from sklearn.cluster import SpectralClustering
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("data/data.csv")
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(df)
+
+X_train, X_test = train_test_split(X_scaled, test_size=0.3, random_state=42)
+
+model = SpectralClustering(n_clusters=2)
+model.fit(X_train)
+
+similarities = model.fit_predict(X_test)
+
+X_train, X_test = scaler.inverse_transform(X_train), scaler.inverse_transform(X_test)
+
+print(f"X_train-> {X_train}")
+print(f"X_test-> {X_test}")
+
+for i, s in enumerate(similarities):
+    print(f'{X_train[s]}->{X_test[i]}')
